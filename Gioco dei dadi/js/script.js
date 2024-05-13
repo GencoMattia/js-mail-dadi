6575580d863649287f472f14630b5bc6070730fb
GencoMattia <gencomattia@hotmail.it>
2024-05-13 16:30:49 +0200

    implemented if structure to compare user and coputer numbers giving messages in console.log

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,5 +1,18 @@
 
-for (let i = 0; i <= 10; i++) {
-    let userNumber = Math.floor(Math.random() * 6) + 1;
-    console.log (userNumber)
+// for (let i = 0; i <= 10; i++) {
+//     let userNumber = Math.floor(Math.random() * 6) + 1;
+//     console.log (userNumber)
+// }
+
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log (userNumber + " " + computerNumber)
+
+if (userNumber > computerNumber) {
+    console.log ("Il giocatore vince con " + userNumber + " contro " + computerNumber)
+} else if (userNumber < computerNumber) {
+    console.log ("Il computer vince con " + computerNumber + " contro " + userNumber)
+} else if (userNumber === computerNumber) {
+    console.log ("Pareggio con doppio " + userNumber)
 }
