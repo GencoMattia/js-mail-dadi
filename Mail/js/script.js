785aad821391e77d6c1c665a94100a7b97748942
GencoMattia <gencomattia@hotmail.it>
2024-05-13 16:38:16 +0200

    added const user mail and added an if structure to compare userMail to userList elements

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,1 +1,9 @@
-usersList = ["giangiorgio@camelot.cm", "giero@camelot.cm", "garluccio@camelot.cm", "genoveffa@camelot.cm"]
+const usersList = ["giangiorgio@camelot.cm", "giero@camelot.cm", "garluccio@camelot.cm", "genoveffa@camelot.cm"]
+
+const userMail = prompt ("Inserisci la tua mail")
+
+if (usersList.includes (userMail) === true) {
+    console.log ("Le tue credenziali sono state accettate")
+} else {
+    console.log ("Le tue credenziali non sono state riconosciute")
+}
