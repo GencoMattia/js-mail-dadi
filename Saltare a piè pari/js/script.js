b6a9fe58bc2d59974b71f0ffd9983e8c8b698bb1
GencoMattia <gencomattia@hotmail.it>
2024-05-13 16:45:40 +0200

    Added if function insside for function to .push even number elements into the array

--- Saltare a piè pari/js/script.js
+++ Saltare a piè pari/js/script.js
@@ -1,5 +1,11 @@
-let evenFoot = []
+let evenFoot = [];
 
 for (let i = 0; i < 6; i++) {
-    const userNumber = Number.parseInt (prompt ("Digita un numero"), 10)
+    const userNumber = Number.parseInt (prompt ("Digita un numero"), 10);
+
+    if (userNumber % 2 === 0) {
+        evenFoot.push (userNumber)
+    }
+    
+    console.log (evenFoot)
 }
